9c1c17fd3df8a23df6bdb4ada1a6a40dcf5a23e2
txzira <txziraio@gmail.com>
2023-08-31 16:01:06 -0700

    first

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from "@angular/core";
+
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+})
+export class HomeComponent implements OnInit {
+  cols = 3;
+  category: string | undefined;
+  rowHeight = ROWS_HEIGHT[this.cols];
+
+  constructor() {}
+  ngOnInit(): void {}
+
+  onColumnsCountChange(colsNum: number): void {
+    this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
+  }
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+  }
+}
